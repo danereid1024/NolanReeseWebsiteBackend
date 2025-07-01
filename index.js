31543b0536102e92aa88df37e79945fefbfea0a8
Dane Reid <forestlevelgames@gmail.com>
2025-07-01 18:00:58 -0400

    deploying backend

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Music API');
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;;
 app.listen(PORT, () => {
   
   console.log(`Server running on http://localhost:${PORT}`);
